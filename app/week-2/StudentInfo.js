aeb1ec024162a296e8d0b527084245fa54cd8f20
kylle710 <KylleFrancis.Bangeng@edu.sait.ca>
2026-01-22 08:51:10 -0700

    Changes

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,16 @@
+export function MyName() {
+    return <h1>Name: Kylle B.</h1>;
+}
+
+import Link from "next/link";
+
+export function GitLink() {
+    return (
+    <p>
+      GitHub:{" "}
+      <Link href="https://github.com/kylle710/cprg306-assignments">
+        kylle710/cprg306-assignments
+      </Link>
+    </p>
+  );
+}
---
+++ app/week-2/page.js
@@ -0,0 +1,11 @@
+import { MyName, GitLink } from "./StudentInfo.js";
+
+export default function Page() {
+  return (
+    <div>
+        <h1>Shopping List</h1>
+        <MyName />
+        <GitLink />
+    </div>
+  );
+}
