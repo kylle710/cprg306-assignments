57631c7241c59a0e39f86aaacfdd5bf081b91d6a
kylle710 <KylleFrancis.Bangeng@edu.sait.ca>
2026-01-28 13:29:50 -0700

    week-3

---
+++ app/week-3/GroceryItem.js
@@ -0,0 +1,9 @@
+export default function Item({ name, quantity, category }) {
+  return (
+    <li className="border p-2 rounded">
+      <p className="font-medium">{name}</p>
+      <p>Quantity: {quantity}</p>
+      <p className="capitalize">Category: {category}</p>
+    </li>
+  );
+}
---
+++ app/week-3/page.js
@@ -0,0 +1,10 @@
+import GroceryItemList from "./GroceryItemList";
+
+export default function Page() {
+  return (
+    <main className="p-4 max-w-xl mx-auto">
+      <h1 className="text-2xl font-bold mb-3">Shopping List</h1>
+      <ItemList />
+    </main>
+  );
+}
