84d08ff84a57d1b3c83138df2aaafb9ac9e87071
kylle710 <KylleFrancis.Bangeng@edu.sait.ca>
2026-01-28 13:33:35 -0700

    week-3(2)

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <main className="p-4 max-w-xl mx-auto">
       <h1 className="text-2xl font-bold mb-3">Shopping List</h1>
-      <ItemList />
+      <GroceryItemList />
     </main>
   );
 }
