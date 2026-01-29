4c0817b2ea57def904f6c7abb82348a4ad3edfcb
kylle710 <KylleFrancis.Bangeng@edu.sait.ca>
2026-01-29 08:44:57 -0700

    back

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,8 +2,8 @@ import GroceryItemList from "./GroceryItemList";
 
 export default function Page() {
   return (
-    <main>
-      <h1>Shopping List</h1>
+    <main className="p-4 max-w-xl mx-auto">
+      <h1 className="text-2xl font-bold mb-3">Shopping List</h1>
       <GroceryItemList />
     </main>
   );
