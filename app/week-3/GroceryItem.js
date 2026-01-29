c71bef047c6972424151411bca8c7870be3fc7f9
kylle710 <KylleFrancis.Bangeng@edu.sait.ca>
2026-01-29 09:00:31 -0700

    hm

--- app/week-3/GroceryItem.js
+++ app/week-3/GroceryItem.js
@@ -1,7 +1,7 @@
 export default function Item({ name, quantity, category }) {
   return (
     <li className="border p-2 rounded bg-green-900 hover:bg-gray">
-      <span className="uppercase text-sm text-gray-500">Name</span>: {name}
+      <span className="uppercase text-sm text-gray-500"></span>: {name}
       <p>Quantity: {quantity}</p>
       <p className="capitalize">Category: {category}</p>
     </li>
