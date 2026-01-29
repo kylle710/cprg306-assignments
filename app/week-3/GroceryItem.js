db13562843113f836d79b82a9ee2fecfcf148dd3
kylle710 <KylleFrancis.Bangeng@edu.sait.ca>
2026-01-29 09:20:08 -0700

    Update GroceryItem.js

--- app/week-3/GroceryItem.js
+++ app/week-3/GroceryItem.js
@@ -1,9 +1,9 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <li className="border p-2 rounded bg-green-900 hover:bg-gray">
-      <p className="font-[Poppins]">{name}</p>
-      <p>Quantity: {quantity}</p>
-      <p className="capitalize">Category: {category}</p>
+    <li className="border p-2 rounded bg-green-900 hover:bg-gray-800 text-white">
+      <p className="font-[Poppins] font-semibold">Name: {name}</p>
+      <p className="font-[Poppins]">Quantity: {quantity}</p>
+      <p className="font-[Poppins] capitalize">Category: {category}</p>
     </li>
   );
 }
