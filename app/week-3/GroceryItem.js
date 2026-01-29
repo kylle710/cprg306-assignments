149591cbd039a25e21cde297b414fcb23b874af6
kylle710 <KylleFrancis.Bangeng@edu.sait.ca>
2026-01-29 08:57:19 -0700

    c

--- app/week-3/GroceryItem.js
+++ app/week-3/GroceryItem.js
@@ -1,6 +1,6 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <li className="border p-2 rounded bg-green-1000 hover:bg-gray">
+    <li className="border p-2 rounded bg-green-900 hover:bg-gray">
       <p className="font-medium">{name}</p>
       <p>Quantity: {quantity}</p>
       <p className="capitalize">Category: {category}</p>
