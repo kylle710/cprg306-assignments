bdcf224218b56943681260ce7fddcf6f3c9399f0
kylle710 <KylleFrancis.Bangeng@edu.sait.ca>
2026-03-18 16:00:45 -0600

    linkchange

--- app/week-9/shopping-list/page.js
+++ app/week-9/shopping-list/page.js
@@ -2,8 +2,8 @@
 
 import { useState } from "react";
 import { useUserAuth } from "../contexts/AuthContext";
-import ItemList from "./item-list";
-import NewItem from "./new-item";
+import ItemList from "../item-list";
+import NewItem from "../new-item";
 import itemsData from "./items.json";
 import Link from "next/link";
 
