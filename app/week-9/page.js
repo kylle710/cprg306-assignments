43593e32be37a440554ab01a515c0374c40da51e
kylle710 <KylleFrancis.Bangeng@edu.sait.ca>
2026-03-26 12:00:20 -0600

    Update page.js

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -34,18 +34,28 @@ export default function Page() {
     <main className="p-4">
       <h1 className="text-2xl font-bold mb-4">Shopping List App</h1>
 
-      {user ? (
-      <section>
-        <p>Welcome, {user.displayName} ({user.email})</p>
-      </section>
+      {user ? 
+      (
+        <section>
+          <p className="mb-2">
+            Welcome, {user.displayName} ({user.email})
+          </p>
+          <div className="flex flex-col gap-2">
+            <Link href="/week-9/shopping-list" className="text-blue-500 hover:underline">
+                Continue to your Shopping List
+            </Link>
+            <button onClick={handleLogout} className="bg-red-500 text-white px-4 py-2 rounded w-max">
+                Log Out
+            </button>
+          </div>
+        </section>
       ) : (
-      <section>
-        <p className="mb-4 text-gray-400">Please sign in to continue to your shopping list.</p>
-        <button onClick={handleLogin} className="bg-blue-600 text-white px-4 py-2 rounded">
-          Login with GitHub
-        </button>
-      </section>
-    )}
-        </main>
+        <section>
+          <button onClick={handleLogin} className="bg-blue-600 text-white px-4 py-2 rounded">
+            Login with GitHub
+          </button>
+        </section>
+      )}
+    </main>
   );
 }
